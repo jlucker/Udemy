174c91547d6ddb726f444010cc4283c8f46c09c7
Justin Lucker <jlucker@users.noreply.github.com>
2017-12-04 14:26:09 -0700

    Components Before

---
+++ vuejs-2-the-complete-guide/Components After/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer><app-footer>
+    </div>
+</template>
+
+<script>
+</script>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    import Servers from './Servers.vue';
+    import ServerDetails from './ServerDetails.vue';
+
+    export default{
+        components:{
+            'app-header': Header,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
+<style>
+
+</style>
