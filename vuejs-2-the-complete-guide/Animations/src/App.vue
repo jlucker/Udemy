bdbd41d8884a444c1b3567be028af302dd4d9484
Justin Lucker <jlucker@users.noreply.github.com>
2018-01-17 14:19:38 -0700

    Update to Animations

--- vuejs-2-the-complete-guide/Animations/src/App.vue
+++ vuejs-2-the-complete-guide/Animations/src/App.vue
@@ -59,6 +59,22 @@
                 <transition name="fade" mode="out-in">
                     <component :is="selectedComponent"></component>
                 </transition>
+                <br>
+                <hr>
+                <br>
+                <button class="btn btn-primary" @click="addItem">Add Item</button>
+                <br>
+                <ul class="list-group">
+                    <transition-group name="slide">
+                        <li
+                                class="list-group-item"
+                                v-for="(number, index) in numbers"
+                                @click="removeItem(index)"
+                                style="cursor: pointer"
+                                :key="number">{{ number }}
+                        </li>
+                    </transition-group>
+                </ul>
             </div>
         </div>
     </div>
@@ -75,7 +91,8 @@
                 load: true,
                 alertAnimation: 'fade',
                 elementWidth: 100,
-                selectedComponent: 'app-success-alert'
+                selectedComponent: 'app-success-alert',
+                numbers: [1, 2, 3, 4, 5]
             }
         },
         methods: {
@@ -125,6 +142,13 @@
             },
             leaveCancelled(el){
                 console.log('leaveCancelled');
+            },
+            addItem(){
+                const pos = Math.floor(Math.random() * this.numbers.length);
+                this.numbers.splice(pos, 0, this.numbers.length + 1);
+            },
+            removeItem(index){
+                this.numbers.splice(index, 1);
             }
         },
         components: {
@@ -171,6 +195,11 @@
         animation: slide-out 1s ease-out forwards;
         transition: opacity 1s;
         opacity: 0;
+        position: absolute;
+    }
+
+    .slide-move {
+        transition: transform 1s;
     }
 
     @keyframes slide-in{
