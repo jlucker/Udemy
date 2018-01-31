027e165767a3b5454de31f569b07409e702c83ce
Justin Lucker <jlucker@users.noreply.github.com>
2018-01-31 11:53:49 -0700

    Axios demo final

---
+++ vuejs-2-the-complete-guide/axios-demo/src/axios-auth.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'https://vue-axios-demo-aebf0.firebaseio.com/'
+})
+
+instance.defaults.headers.common['SOMETHING'] = 'something'
+
+export default instance
