86b736a6d0be37cfc0698a094990ce419d2912f6
Justin Lucker <jlucker@users.noreply.github.com>
2018-02-01 09:57:22 -0700

    vue-auth added authentication

--- vuejs-2-the-complete-guide/vue-auth/src/components/auth/signin.vue
+++ vuejs-2-the-complete-guide/vue-auth/src/components/auth/signin.vue
@@ -38,7 +38,8 @@
           email: this.email,
           password: this.password,
         }
-        console.log(formData)
+        console.log(formData);
+        this.$store.dispatch('login', {email: formData.email, password: formData.password})
       }
     }
   }
