67c85da0518a6c345412deee44c7ad00a0f11730
Justin Lucker <jlucker@users.noreply.github.com>
2019-03-18 10:39:41 -0700

    Starting shopping cart feature

---
+++ GraniteHouse/GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Extensions;
+using GraniteHouse.Models;
+using GraniteHouse.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartVM { get; set; }
+
+        public ShoppingCartController( ApplicationDbContext db)
+        {
+            _db = db;
+            ShoppingCartVM = new ShoppingCartViewModel()
+            {
+                Products = new List<Models.Products>()
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
